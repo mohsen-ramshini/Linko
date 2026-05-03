2455b6b753b079c3d4558869cfdf60ab380466a2
mohsen_ramshini <rsmohsen20@gmail.com>
2026-05-03 21:13:15 +0330

    fix(dotenv): install dotenv and setup project

--- apps/linko_backend/config/db.js
+++ apps/linko_backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONOGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Mongodb connected successfully");
   } catch (err) {
     console.log("Mongodb connection error", err.message);
--- apps/linko_backend/index.js
+++ apps/linko_backend/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+
+// IMPORT ROUTES
+const userRoutes = require("./routes/userRoutes");
+
+dotenv.config();
+
 const PORT = 3000;
 
 const app = express(morgan("dev"));
