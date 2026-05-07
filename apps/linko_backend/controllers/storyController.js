7241252ec458a1d5b9467c8f8d1bafb3c1631eeb
mohsen_ramshini <rsmohsen20@gmail.com>
2026-05-07 15:47:55 +0330

    feat(story): Implement story routes

---
+++ apps/linko_backend/controllers/storyController.js
@@ -0,0 +1,33 @@
+exports.getStories = (req, res) => {
+  res.json({ message: "getStories" });
+};
+exports.createStory = (req, res) => {
+  res.json({ message: "createStory" });
+};
+exports.getStoryById = (req, res) => {
+  res.json({ message: "getStoryById" });
+};
+exports.deleteStory = (req, res) => {
+  res.json({ message: "deleteStory" });
+};
+exports.markStoryAsViewed = (req, res) => {
+  res.json({ message: "markStoryAsViewed" });
+};
+exports.getStoryViewers = (req, res) => {
+  res.json({ message: "getStoryViewers" });
+};
+exports.replyToStory = (req, res) => {
+  res.json({ message: "replyToStory" });
+};
+exports.addToHighlight = (req, res) => {
+  res.json({ message: "addToHighlight" });
+};
+exports.getMyHighlights = (req, res) => {
+  res.json({ message: "getMyHighlights" });
+};
+exports.getUserHighlights = (req, res) => {
+  res.json({ message: "getUserHighlights" });
+};
+exports.getStoryArchive = (req, res) => {
+  res.json({ message: "getStoryArchive" });
+};
