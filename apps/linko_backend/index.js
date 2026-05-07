d636f9571624306625a4b8b888962c5f9291f3ec
mohsen_ramshini <rsmohsen20@gmail.com>
2026-05-07 19:08:45 +0330

    feat(route): Update Imported routes

--- apps/linko_backend/index.js
+++ apps/linko_backend/index.js
@@ -5,6 +5,17 @@ const dotenv = require("dotenv");
 
 // IMPORT ROUTES
 const userRoutes = require("./routes/userRoutes");
+const userRoutes = require("./routes/userRoutes");
+const postRoutes = require("./routes/postRoutes");
+const commentRoutes = require("./routes/commentRoutes");
+const followRoutes = require("./routes/followRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const storyRoutes = require("./routes/storyRoutes");
+const notificationRoutes = require("./routes/notificationRoutes");
+const hashtagRoutes = require("./routes/hashtagRoutes");
+const searchRoutes = require("./routes/searchRoutes");
+const reportRoutes = require("./routes/reportRoutes");
+const adminRoutes = require("./routes/adminRoutes");
 
 dotenv.config();
 
@@ -19,6 +30,16 @@ app.use(express.json());
 connectDB();
 
 app.use("/api/v1/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/follow", followRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/stories", storyRoutes);
+app.use("/api/notifications", notificationRoutes);
+app.use("/api/hashtags", hashtagRoutes);
+app.use("/api/search", searchRoutes);
+app.use("/api/reports", reportRoutes);
+app.use("/api/admin", adminRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
